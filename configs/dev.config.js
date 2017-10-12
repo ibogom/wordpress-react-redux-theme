5ec3aa6edd3d6d1a19bb77f3243528fcb7fa1349
Ievgen <ievgen.bogomolov@gmail.com>
2017-10-12 22:36:06 +0300

    dev server implementation

--- configs/dev.config.js
+++ configs/dev.config.js
@@ -3,23 +3,7 @@ const path = require('path');
 
 const devConfig = Object.assign({}, baseConfig, {
 
-    devtool: "#eval-source-map",
-
-    entry: {
-        main: [
-            './js/main.js',
-            'react-hot-loader/patch',
-            'webpack-hot-middleware/client',
-        ]
-    },
-
-    devServer: {
-        contentBase: path.join(__dirname, "../src"),
-        host: 'localhost',
-        port: 3000,
-        historyApiFallback: true,
-        hot: true,
-    },
+    devtool: "#eval-source-map"
 
 });
 
